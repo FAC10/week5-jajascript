a3623de7a6a370147c6d2315fa6b1e0f3ec376c0
Alexis Lui <alexislui@hotmail.co.uk>
2017-03-23 18:08:24 +0000

    Changing seconds to Minutes and Secs

--- public/main.js
+++ public/main.js
@@ -14,15 +14,47 @@ xhr.open(method, url, true);
 xhr.send();
 }
 
+function secToMin(time) {
+  var s = time%60;
+  var m = Math.floor(time/60)
+  return `${m}:${s}`;
+}
+
 function appendData(data) {
-  console.log(data);
-  //   var journey = document.getElementById('journey');
-  //   journey.innerHTML = '';
+  // console.log(data);
+  var westResults = document.getElementById('westbound-data');
+  var eastResults = document.getElementById('eastbound-data');
+  var long = Math.max(data.west.length, data.east.length);
+  var short = Math.min(data.west.length, data.east.length);
+  // var diff = long - short;
+  // while (long > short) {
   //
-  //   data.forEach(function(val) {
-  //     var option = document.createElement('li');
-  //     option.className += 'option-li';
-  //     option.innerHTML = val[1];
-  //     journey.appendChild(option);
-  // });
+  // }
+
+
+  westResults.innerHTML = '';
+
+  data.west.forEach((e) => {
+    var westDestination = document.createElement('div');
+    westDestination.className = 'west-dest-unit';
+    westDestination.innerHTML = e[1] + ' s';
+    westResults.appendChild(westDestination);
+
+    var westDepartures = document.createElement('div');
+    westDepartures.className = 'west-dep-unit';
+    westDepartures.innerHTML = secToMin(e[2]);
+    westResults.appendChild(westDepartures);
+  });
+
+  data.east.forEach((e) => {
+    var eastDestination = document.createElement('div');
+    eastDestination.className = 'east-dest-unit';
+    eastDestination.innerHTML = e[1] + ' s';
+    eastResults.appendChild(eastDestination);
+
+    var eastDepartures = document.createElement('div');
+    eastDepartures.className = 'east-dep-unit';
+    eastDepartures.innerHTML = secToMin(e[2]);
+    eastResults.appendChild(eastDepartures);
+  });
 }

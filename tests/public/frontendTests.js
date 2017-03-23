34f84bf3dce5fd2e40bab4b82b3fa72f46cdaa72
Joey Scott <joannascott29@hotmail.co.uk>
2017-03-23 23:28:08 +0000

    Add two tests to check secToMin function is working correctly Relates #44

--- tests/public/frontendTests.js
+++ tests/public/frontendTests.js
@@ -3,15 +3,18 @@ QUnit.test('2 equals 2', function(assert) {
   assert.equal(2, 2, '2 is equal to 2');
 });
 
-  // QUnit.test('Test that getDataUrl returns correct URL', function(assert) {
-  //   assert.equal(getDataUrl('barack'), 'https://nobel-laureate-autocomplete.herokuapp.com/search/barack', 'Yay, our test works!');
-  // });
-  //
-  // QUnit.test('Test that getUserInput returns correct userInput', function(assert) {
-  //     var event = {
-  //       target: {
-  //         value: 'bar'
-  //       }
-  //     }
-  //     assert.equal(getUserInput(event), 'bar', 'Yay, our test works!');
-  //   });
+QUnit.test('Test that secToMin returns the correct output, input = 121', function(assert) {
+  assert.equal(secToMin(121), '2:01', '121 returns 2:01! Yay, our test works!');
+});
+
+QUnit.test('Test that secToMin returns the correct output, input = 79', function(assert) {
+  assert.equal(secToMin(79), '1:19', '79 returns 1:19! Yay, our test works!');
+});
+// QUnit.test('Test that getUserInput returns correct userInput', function(assert) {
+//     var event = {
+//       target: {
+//         value: 'bar'
+//       }
+//     }
+//     assert.equal(getUserInput(event), 'bar', 'Yay, our test works!');
+//   });

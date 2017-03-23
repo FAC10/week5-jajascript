ac20a77e5d515f5747b34e7d562bb66a76f20c71
Joey Scott <joannascott29@hotmail.co.uk>
2017-03-23 23:08:24 +0000

    Add public tests folder Containing qunit.html and frontendTests.js Add example test to check QUnit is working Relates #44

---
+++ tests/public/frontendTests.js
@@ -0,0 +1,17 @@
+// Test to check QUnit is working
+QUnit.test('2 equals 2', function(assert) {
+  assert.equal(2, 2, '2 is equal to 2');
+});
+
+  // QUnit.test('Test that getDataUrl returns correct URL', function(assert) {
+  //   assert.equal(getDataUrl('barack'), 'https://nobel-laureate-autocomplete.herokuapp.com/search/barack', 'Yay, our test works!');
+  // });
+  //
+  // QUnit.test('Test that getUserInput returns correct userInput', function(assert) {
+  //     var event = {
+  //       target: {
+  //         value: 'bar'
+  //       }
+  //     }
+  //     assert.equal(getUserInput(event), 'bar', 'Yay, our test works!');
+  //   });
